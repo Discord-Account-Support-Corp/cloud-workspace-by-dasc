1c63d409608714acc807e444f5592da4384d391e
@UhLukas <119970521+UhLukas@users.noreply.github.com>
2025-07-11 18:14:59 +0300

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import Editor from './modules/docs/components/Editor';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Cloud Workspace | Docs</h1>
+      <Editor />
+    </div>
+  );
+}
